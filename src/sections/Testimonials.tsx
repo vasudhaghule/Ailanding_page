79d40d54997a0eff8e01be372c040d6f370c3661
Vasudha Ghule <133812225+vasudhaghule@users.noreply.github.com>
2024-11-24 21:37:34 +0530

    Update Testimonials.tsx

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -5,6 +5,7 @@ import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
 import avatar4 from "@/assets/avatar-4.png";
 import {motion} from "framer-motion";
+
 const testimonials = [
   {
     text: "This product has completely transformed how I manage my projects and deadlines",
@@ -40,23 +41,24 @@ export const Testimonials = () => {
           Beyond Expectations
         </h2>
         <p className="text-white/70 text-lg md:text-xl text-center mt-5 tracking-tight max-w-sm mx-auto">
-          Our revolutionary AI SEO tools have transformed our clients' strategies.
+          Our revolutionary AI SEO tools have transformed our clients&apos; strategies.
         </p>
         <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
           <motion.div 
-          initial={{
-            translateX:'-50%'
-          }}
-          animate={{
-            translateX:"0",
-          }}
-          transition={{
-            repeat:Infinity,
-            ease:"linear",
-            duration:30,
-          }}
-          className="flex gap-5 pr-5 flex-none ">
-            {[...testimonials,...testimonials] .map((testimonial) => (
+            initial={{
+              translateX: '-50%'
+            }}
+            animate={{
+              translateX: "0",
+            }}
+            transition={{
+              repeat: Infinity,
+              ease: "linear",
+              duration: 30,
+            }}
+            className="flex gap-5 pr-5 flex-none"
+          >
+            {[...testimonials, ...testimonials].map((testimonial) => (
               <div
                 key={testimonial.name}
                 className="border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,0.3),black)] max-w-xs md:max-w-md flex-none"
